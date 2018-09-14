96cebfd7985f2523c3449cd7d0cecddf6500ac35
Mandy Pilaud <mandyypilaud@gmail.com>
2018-09-14 16:32:23 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@
 
 board =  [" "," "," "," "," "," "," "," "," "]
 def display_board (board)
-  puts "   |   |   "
+  puts "#{board[1]}|#{board[2]|#{board[3]}"
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
